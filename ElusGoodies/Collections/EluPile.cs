6632a211ea0af50ba46035d8bbf344fc1f5839d1
Elusive239 <thomasmcguff2727@gmail.com>
2021-08-28 15:25:57 -0400

    Quick Commit Through Alias

--- ElusGoodies/Collections/EluPile.cs
+++ ElusGoodies/Collections/EluPile.cs
@@ -14,13 +14,17 @@ namespace ElusGoodies.Collections
             items = new T[size];
             Count = 0;
         }
-
+        /// <summary>
+        /// Returns true if the item is added to the list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Add(T item){
             if(Contains(item))
             foreach (T t in this)
             {
-                if(t.Equals(item) && t.itemAmount  < t.itemCapacity){
-                    t.itemAmount++;
+                if(t.Equals(item) && (t.itemAmount  < t.itemCapacity || t.itemCapacity == NaN)){
+                    t.itemAmount+=item.itemAmount;
                     return true;
                 }
             }
@@ -30,20 +34,64 @@ namespace ElusGoodies.Collections
             }
     
             items[Count] = item;
-            item.itemAmount++;
             Count++;    
         }
-
+        /// <summary>
+        /// Returns true if the provided item is in the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Contains(T item){
             if(Count <= 0) return false;
+            return GetIndex(item) != -1;
+        }
 
-            foreach (T t in this)
-            {
-                if(t.Equals(item)){
-                    return true;
+        /// <summary>Removes an item from the list at the specified index.</summary>
+        public void Remove(int i){
+            for(int x = i; x < Count;x++){
+                if(x+1 != Count){
+                    items[x] = items[x+1];
                 }
             }
-            return false;
+            Count --;
+        }
+
+        /// <summary>Removes the specified item from the list.</summary>
+        public void Remove(T item){
+            int i = GetIndex(item);
+            for(int x = i; x < Count;x++){
+                if(x+1 != Count){
+                    items[x] = items[x+1];
+                }
+            }
+            Count --;
+        }
+        /// <summary>
+        /// Removes an amount of an item from the item in the list.
+        /// returns false if the item is not within the list or the
+        /// amount you are attempting to remove is greater than the 
+        /// amount the item has.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public bool RemoveAmount(T item, int amount){
+            int index = GetIndex(item);
+            if(index == -1) return false;
+
+            if(item.itemAmount - amount < 1) return false;
+
+            items[index].itemAmount -= amount;
+
+        }
+
+        public int GetIndex(T item){
+            for(int i = 0; i < Count; i++){
+                if(items[i].Equals(item)){
+                    return i;
+                }
+            }
+            return -1;
         }
 
         //IEnumerator and IEnumerable require these methods.
@@ -81,6 +129,17 @@ namespace ElusGoodies.Collections
             } 
         }
 
+        /// <summary>
+        /// Removes items from the list if they're itemAmount is == 0
+        /// </summary>
+        public void UpdateItem(T item){            
+        int index = GetIndex(item);
+            if(index == -1) return;
+
+            if(items[index].itemAmount < 1)
+                Remove(index);
+        }
+        
     }
     /// <summary>
     /// Required to be implemented for an item to be put in a pile.
