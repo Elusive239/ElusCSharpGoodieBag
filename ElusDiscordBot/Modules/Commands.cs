995110708a0ebb96aa080dc627c2b4163a63a77b
Elusive239 <thomasmcguff2727@gmail.com>
2021-09-07 17:49:36 -0400

    Quick Commit Through Alias

--- ElusDiscordBot/Modules/Commands.cs
+++ ElusDiscordBot/Modules/Commands.cs
@@ -1,3 +1,4 @@
+using System;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 namespace ElusDiscordBot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>{
+
+        static Random randomness = new Random();
 
         [Command("ping")]
         public async Task Ping() => await ReplyAsync("pong");
@@ -19,6 +22,39 @@ namespace ElusDiscordBot.Modules
             }else{
                 //await ReplyAsync("<@"+ user + "> you a bish.");
                 await Context.Channel.SendMessageAsync(user.Mention + " u a bish.");
+            }
+        }
+
+        [Command("duel")]
+        public async Task TimeToFight(SocketGuildUser user1, SocketGuildUser user2){
+            if(user1 == null || user2 == null){
+                await ReplyAsync("Requires two users as inputs.");
+                return;
+            }
+            DateTime dt = new DateTime();
+
+            long startTime = dt.Millisecond;
+            long currentTime = startTime;
+            long endTime = startTime + 3000001;
+
+            while (currentTime < endTime){
+                currentTime++;
+                if(currentTime - startTime == 1000000){
+                    await ReplyAsync("One...");
+                }else if(currentTime - startTime == 2000000){
+                    await ReplyAsync("Two...");
+                }else if(currentTime - startTime == 3000000){
+                    await ReplyAsync("Three...");
+                }
+            }   
+            await ReplyAsync("DRAW!");
+
+            bool rand = (randomness.Next(20) + 1) / 10 == 1;
+
+            if(rand){
+                await ReplyAsync(user1.Mention + " drew first! GG!");
+            }else {
+                await ReplyAsync(user2.Mention + " drew first! GG!");
             }
         }
     }   
